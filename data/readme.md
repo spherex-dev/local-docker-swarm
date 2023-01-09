ec965bf8d9ac7d30bd8712122638651ab3a12b51
Justin Ho <96013445+spherex-dev@users.noreply.github.com>
2023-01-09 18:40:46 +0900

    First commit

---
+++ data/readme.md
@@ -0,0 +1,7 @@
+directories:
+
+daemon1
+daemon2
+daemon3
+
+may need to be created by hand to start
---
+++ start-daemons.sh
@@ -0,0 +1,3 @@
+sudo dockerd --config-file ./daemons/daemon1.json &
+sudo dockerd --config-file ./daemons/daemon2.json &
+sudo dockerd --config-file ./daemons/daemon3.json &
